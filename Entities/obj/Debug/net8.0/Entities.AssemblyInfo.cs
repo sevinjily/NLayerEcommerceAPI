d23f54d1dc6f201e21a2803fec0ba5bf6f266420
svncmsva <sevincmusaeva01@gmail.com>
2025-02-06 00:03:40 +0400

    productDAL

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8251e6052216cd26289f7486cc19623a80e23aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d20155edca1ef4f5c0e3d0aebcc947369db8cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
